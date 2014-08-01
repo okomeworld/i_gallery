ce261b66565ef34fac1a13063733c9c73766e670
kawabataryo <ryo.wizard@gmail.com>
2014-08-01 18:01:00 +0900

    拡大画像のページ送り機能を実�

--- inc/js/app.gallery.js
+++ inc/js/app.gallery.js
@@ -13,19 +13,50 @@ App.Gallery = (function(window,$){
 			$document.on('click', '.expand', function(e) {
 				e.preventDefault();
 				var $self = $(this);
+				$self.parent().parent().addClass('current');
 				that.addImg($self);
 			});
 
-			$document.on('click', '.exBox', function(e) {
+			$document.on('click', '.exBox img', function(e) {
 				e.preventDefault();
-				var $self = $(this);
+				var $self = $(this).parent().parent();
+				$('.current').removeClass('current');
 				that.removeImg($self);
 			});
+
+			$document.on('click', '.next', function(e) {
+				e.preventDefault();
+				that.sendPage('next');
+			});
+
+			$document.on('click', '.prev', function(e) {
+				e.preventDefault();
+				that.sendPage('prev');
+			});
+
+		},
+
+		sendPage: function(send){
+			var $current = $('.current');
+			if(send === 'next'){
+				var $send = $current.next();
+				var $sendA = $current.next().find('a');
+			}else if(send === 'prev'){
+				var $send = $current.prev();
+				var $sendA = $current.prev().find('a');
+			}
+			$('.exBox').remove();
+
+			this.addImg($sendA);
+
+			$current.removeClass('current');
+			$send.addClass('current');
 		},
 
 		addImg: function($self){
 			var $window = $(window);
 			var $body = $('body');
+			var $currentBox = $self.parent().parent();
 			var src = $self.attr('href');
 			var y = $window.scrollTop();
 			var w_height = $window.height();
@@ -36,7 +67,7 @@ App.Gallery = (function(window,$){
 
 			$body.append(
 				'<div class="exBox" style="height:' + b_height + 'px;">'
-				+ '<img src="' + src + '" />'
+				+ '<div><a href="#" class="btn prev">PREV</a><a href="#" class="btn next">NEXT</a><img src="' + src + '" /></div>'
 				+ '</a>'
 			);
 
@@ -48,8 +79,16 @@ App.Gallery = (function(window,$){
 					var value = y;
 					var img_height = w_height;
 				}
-				$self.css({marginTop: value,height: img_height}).addClass('anime_in');
+				$('.exBox').css({paddingTop: value});
+				$self.css({height: img_height}).addClass('anime_in');
 			});
+
+			if(!$currentBox.next().size()){
+				$('.next').hide();
+			}else if(!$currentBox.prev().size()){
+				$('.prev').hide();
+			}
+
 		},
 
 		removeImg: function($self){
